cdda4260ec6c742e325d8e89723d2a39964f0582
Adiputra <tuts.adiputra@gmail.com>
2019-06-30 17:32:08 +0700

    php70: scalar type hint

---
+++ src/php-70/scalar-type-hint/scalar-type-hint-strict-false.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 0);
+
+function addUser(string $name, int $age, bool $active)
+{
+    print $name . ' ' . $age . ' ' . $active;
+}
+
+addUser('First user', 30, true);
+
+addUser('Second user', '30', false);
